cee0c57dd059d3d182bab9ed3605908d6a6d626e
Benjamin DERVILLE <benderville@gmail.com>
2025-02-17 13:21:04 +0100

    update

--- service/migrations/versions/bdcf2c6aa3a6_create_sample_database.py
+++ service/migrations/versions/bdcf2c6aa3a6_create_sample_database.py
@@ -9,6 +9,7 @@ from typing import Sequence, Union
 
 import sqlalchemy as sa
 from alembic import op
+
 from data.sample.create import create_sample_database, delete_sample_database
 
 # revision identifiers, used by Alembic.
