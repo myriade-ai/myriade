fad9940c22817b03c63499872dfae2642e68fcd4
Benjamin DERVILLE <benderville@gmail.com>
2025-02-17 18:25:49 +0100

    fix: build

--- view/src/main.ts
+++ view/src/main.ts
@@ -15,7 +15,7 @@ import router from './router'
 import Vue3TouchEvents, { type Vue3TouchEventsOptions } from 'vue3-touch-events'
 import { clerkPlugin } from '@clerk/vue'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = "pk_test_ZXBpYy10aWdlci00Ny5jbGVyay5hY2NvdW50cy5kZXYk"
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env.local file')
