28985e060125addceaec621f897a07fd2b557a9f
Benjamin DERVILLE <benderville@gmail.com>
2025-03-27 14:56:16 +0100

    fix: logout when 401

--- view/src/plugins/axios.ts
+++ view/src/plugins/axios.ts
@@ -1,10 +1,11 @@
+import { logout } from '@/stores/auth'
 import axios from 'axios'
 
 axios.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      window.location.href = '/login'
+      logout()
     }
     return Promise.reject(error)
   }
