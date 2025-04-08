e9f7ba9855b64054401693b47ee7ce4f2ddee3ab
Benjamin DERVILLE <benderville@gmail.com>
2025-04-08 15:34:40 +0200

    build: update deploy script

--- update_servers.sh
+++ update_servers.sh
@@ -10,15 +10,23 @@ COMMANDS=$(cat <<'EOF'
 EOF
 )
 
-# Fetch all instances' names in the specified zone
-servers=$(gcloud compute instances list --format="value(name)")
+# Check if a server list is provided as an argument
+if [ -n "$1" ]; then
+  # Use the provided list, splitting by comma
+  IFS=',' read -r -a servers <<< "$1"
+else
+  # Fetch all instances' names in the specified zone if no argument is provided
+  servers=$(gcloud compute instances list --format="value(name)")
+fi
 
 echo "List of servers that will be updated:"
-for server in $servers; do
+for server in "${servers[@]}"; do
   echo "- $server"
 done
 
-for server in $servers; do
+for server in "${servers[@]}"; do
+  echo "Copying docker-compose.yml to instance: $server"
+  gcloud compute scp ada/docker-compose.yml "$server":~/docker-compose.yml
   echo "Executing command on instance: $server"
   gcloud compute ssh "$server" --command="$COMMANDS"
 done
