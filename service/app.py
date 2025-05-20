f37e5ab6317f85ed7b951c50dc4f219a556dbd7c
Benjamin DERVILLE <benderville@gmail.com>
2025-05-20 13:07:33 +0200

    move query apis to back folder

--- service/app.py
+++ service/app.py
@@ -27,14 +27,14 @@ def create_app():
     app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
     app.json.sort_keys = False
 
-    from ai.api import api as ai_api
     from auth.api import api as auth_api
     from back.api import api as back_api
+    from back.query import api as query_api
     from chat.api import api as chat_api
 
     app.register_blueprint(chat_api)
     app.register_blueprint(back_api)
-    app.register_blueprint(ai_api)
+    app.register_blueprint(query_api)
     app.register_blueprint(auth_api)
 
     @app.before_request
