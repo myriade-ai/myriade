049f2ba38662aa673e5f4f9c6f35e975d32bd7f4
Benjamin DERVILLE <benderville@gmail.com>
2025-09-29 09:07:59 +0200

    fix: migration

--- service/migrations/versions/9addce233509_add_dbt_repo_path_to_database_table.py
+++ service/migrations/versions/9addce233509_add_dbt_repo_path_to_database_table.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "9addce233509"
-down_revision: Union[str, None] = "c3dae8db7eae"
+down_revision: Union[str, None] = "0f8a4c77fd2f"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
