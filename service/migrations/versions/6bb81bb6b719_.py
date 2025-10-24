00f36336a8ae4d9cfe9cc323c9d0d9f51a667168
narchok <jabin.julian@gmail.com>
2025-10-24 17:41:57 +0200

    feat: implement background metadata sync for databases with status tracking

---
+++ service/migrations/versions/6bb81bb6b719_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 6bb81bb6b719
+Revises: 17079fd7073b
+Create Date: 2025-10-24 14:14:07.775557
+
+"""
+
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = "6bb81bb6b719"
+down_revision: Union[str, None] = "17079fd7073b"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
